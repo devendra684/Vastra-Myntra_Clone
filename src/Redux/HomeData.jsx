3b0b6daaae245f16f6d256ec71fdbaa587f52816
ravikr2 <batmanxjoker2@gmail.com>
2023-02-23 21:59:21 +0530

    day-3

--- src/Redux/HomeData.jsx
+++ src/Redux/HomeData.jsx
@@ -1,9 +1,47 @@
 import axios from "axios";
 
-export const HomeData = async() => {
-// let res= await axios.get("http://localhost:3004/data")
-let res= await axios.get("https://vastra.onrender.com/HomePageData")
+export const HomeData = async () => {
+  // let res= await axios.get("http://localhost:3004/data")
+  let res = await axios.get("https://vastra.onrender.com/HomePageData");
 
-  return res.data
-
+  return res.data;
 };
+
+export const slider = [
+  {
+    id: 1,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/2/14/7cec9b95-a683-473c-aca8-cc510821b1cd1676394720493-Desktop-Banner.gif",
+  },
+  {
+    id: 2,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/28/84b6a214-9eb3-49eb-9f9d-72cec56ec5d71659019908592-Indian-Wear_DK--1-.jpg",
+  },
+  {
+    id: 3,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/28/6107d28b-2bcb-44e6-9743-655b54550b8f1659020199598-Workwear_Desk--1-.jpg",
+  },
+  {
+    id: 4,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/28/0174e4d7-448c-4746-8572-69461ad5be101659020268081-Tops---Tees_Desk.jpg",
+  },
+  {
+    id: 5,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/25/b656a7f4-4688-4997-bb7c-54b78793981e1658752386588-Western-Wear_Desk.jpg",
+  },
+  {
+    id: 6,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/25/179e278f-77ee-44c2-bf39-9f00b0cd08e01658752429301-Handbags_Desk.jpg",
+  },
+  {
+    id: 7,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/25/9be788ff-39a4-4214-99d0-fc97505aae5a1658752545685-USPA_Desk_Banner.jpg",
+  },
+  {
+    id: 8,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/31/4031994d-9092-4aa7-aea1-f52f2ae5194f1654006594976-Activewear_DK.jpg",
+  },
+  {
+    id: 9,
+    url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/27/53b4daed-cd2c-4111-86c5-14f737eceb351656325318973-Handbags_Desk.jpg",
+  },
+];
