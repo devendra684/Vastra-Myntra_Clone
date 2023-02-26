b4787f2ba85591fe9583a3979c4e661bdb6b5744
Devendra Singh <devendra.singh248124@gmail.com>
2023-02-26 12:18:22 +0530

    Files Arranged

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import AllRoutes from './Components/AllRoutes';
-import { Navbar } from './Components/Navbar/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 // import { FullDetails } from './Pages/FullDetails';
 // import { LoginOrSignUp } from './Pages/LoginOrSignUp';
 // import {Otp} from './Pages/Otp';
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <AllRoutes />
-      <Navbar /> 
+      {/* <Navbar />  */}
       
       {/* <FullDetails/> */}
       {/* <LoginOrSignUp />  */}
