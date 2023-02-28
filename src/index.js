3e04cc54798a2e90a66f9db4a5553293ef6ee4b6
Devendra Singh <devendra.singh248124@gmail.com>
2023-02-28 13:41:41 +0530

    Final Submission

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ import { store } from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </ChakraProvider>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
