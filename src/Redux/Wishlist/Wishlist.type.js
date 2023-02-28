61626aae6edb70aa08780cb1bb4d879fd8880e29
Devendra Singh <devendra.singh248124@gmail.com>
2023-02-28 13:45:58 +0530

    Final Submission

--- src/Redux/Wishlist/Wishlist.type.js
+++ src/Redux/Wishlist/Wishlist.type.js
@@ -1,5 +1,4 @@
 export const GET_WISHLIST_LOADING = "get/wishlist/loading"
 export const GET_WISHLIST_SUCCESS = "get/wishlist/success"
 export const GET_WISHLIST_ERROR = "get/wishlist/error"
-
-export const DELETE_PRODUCT_SUCCESS = "delete/wishhlist/success"
+export const DELETE_PRODUCT_SUCCESS = "delete/wishlist/success"
